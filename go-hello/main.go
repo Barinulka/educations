fc6a69f40282a305e08b4c1675218dcdb28a0d4f
al.barinov <al.barinov@academcity.online>
2025-04-27 13:07:35 +0300

    fix: some refactor and test

--- go-hello/main.go
+++ go-hello/main.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 
+const englisHelloPrefix = "Hello"
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+
+	return fmt.Sprintf("%s, %s!", englisHelloPrefix, name)
 }
 
 func main() {
--- go-hello/main_test.go
+++ go-hello/main_test.go
@@ -3,9 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Alexey")
-	want := "Hello, Alexey"
+	t.Run("say 'Hello' to people", func(t *testing.T) {
+		got := Hello("Alexey")
+		want := "Hello, Alexey!"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when got empty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if (got != want) {
 		t.Errorf("got %q want %s", got, want)
 	}
